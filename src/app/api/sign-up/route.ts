f2ebbd457bb608520a5582e2220b184b70e11696
Muhammad Ashir Khan <just4fun17678@gmail.com>
2025-04-17 22:40:55 +0500

    Added providers and pages in NextAuth routes

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -38,7 +38,8 @@ export async function POST(request: Request) {
         const hanshedPassword = await bcrypt.hash(password, 10);
         existingUserByEmail.password = hanshedPassword;
         existingUserByEmail.verifyCode = verifyCode;
-        existingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000)
+        existingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000);
+        await existingUserByEmail.save();
       }
     } else {
       const hanshedPassword = await bcrypt.hash(password, 10);
